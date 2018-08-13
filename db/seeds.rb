b7c2f1204db242f8d7e279f38c9070be9035d977
Marine Leaute <leautemarine@yahoo.fr>
2018-08-13 23:06:20 +0200

    some seeds & tof

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,12 @@ marine = User.create!(
   password: "azerty"
 )
 
+enzo = User.create!(
+  name: "Enzo"
+  email: "enzo@gmail.com"
+  password: "azerty"
+  )
+
 puts "Creating rappers..."
 booba = Rapper.create!({
   agent: lisiane,
@@ -45,6 +51,112 @@ drake = Rapper.create!({
   address: "2 Rue meuris, 44100 Nantes, France",
   price_per_day: 4000,
 })
+
+2pac = Rapper.create!({
+  agent: lisiane,
+  blaz: "2pac",
+  description: "mort",
+  punchline: "Thug Life",
+  style: "Hip-hop",
+  origin: "Las Vegas",
+  address: "8, allée de la maison rouge 44000 Nantes, France",
+  price_per_day: 700,
+})
+
+teki = Rapper.create!({
+  agent: marine,
+  blaz: "Teki latex",
+  description: "A travaillé avec Lio et Leslie",
+  punchline: "Quand je me lève le matin",
+  style: "electro",
+  origin: "parisien",
+  address: "24 rue kervegan 44000 Nantes, France",
+  price_per_day: 500,
+})
+
+
+mc_circulaire = Rapper.create!({
+  agent: lisiane,
+  blaz: "Mc Circulaire",
+  description: "Aussi bon que la brioche",
+  punchline: "Vendéen jusqu'au bout des seins",
+  style: "Vendéen",
+  origin: "Vendéen",
+  address: "5 boulevard de Vendée 44200 Nantes, France",
+  price_per_day: 100,
+})
+
+jul = Rapper.create!({
+  agent: enzo,
+  blaz: "Jul",
+  description: "lol",
+  punchline: "On m'appelle l'Ovni",
+  style: "ET",
+  origin: "Marseille",
+  address: "12 avenue des cigales, 44200 Nantes, France",
+  price_per_day: 10,
+})
+
+
+kanye = Rapper.create!({
+  agent: enzo,
+  blaz: "Kanye West",
+  description: "...",
+  punchline: "...",
+  style: "...",
+  origin: "...",
+  address: "5 rue de l'Ouest 44100 Nantes, France",
+  price_per_day: 666,
+})
+
+
+hamza = Rapper.create!({
+  agent: marine,
+  blaz: "Hamza",
+  description: "mi r'n'b mi trap",
+  punchline: "Sauce God",
+  style: "trap'n'b",
+  origin: "Belge",
+  address: "94, rue des hauts pavés 44000 Nantes, France",
+  price_per_day: 1500,
+})
+
+pnl = Rapper.create!({
+  agent: enzo,
+  blaz: "PNL",
+  description: "...",
+  punchline: "Non mais allo",
+  style: "Rap cheulou",
+  origin: "Essone",
+  address: "56 rue de Strasbourg 44000 Nantes, France",
+  price_per_day: 10000,
+})
+
+
+sch = Rapper.create!({
+  agent: marine,
+  blaz: "SCH",
+  description: "hardcore",
+  punchline: "Appart pas loué",
+  style: "marseillais",
+  origin: "Marseille",
+  address: "30 boulevard de la prairie au Duc 44200 Nantes, France",
+  price_per_day: 4000,
+})
+
+ichon = Rapper.create!({
+  agent: lisiane,
+  blaz: "Ichon",
+  description: "Si l'on ride",
+  punchline: "l'amour nous donnera des ailes",
+  style: "rap conscient",
+  origin: "Paris",
+  address: "3 rue des carmes 44000 Nantes, France",
+  price_per_day: 1000,
+})
+
+
+
 
 puts "Creating bookings..."
 Booking.create!({
