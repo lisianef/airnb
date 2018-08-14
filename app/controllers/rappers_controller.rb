6dd3d7874b09fcc9b3d1c5e36722c212dde98b49
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-14 14:14:51 +0200

    début de l'index

---
+++ app/controllers/rappers_controller.rb
@@ -0,0 +1,7 @@
+class RappersController < ApplicationController
+  def index
+    @rappers = Rapper.all
+    # @const_map = new GMaps({ el: '#map', lat: 48.8648482, lng: 2.3798534, zoom: 14 })
+  end
+
+end
