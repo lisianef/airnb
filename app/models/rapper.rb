50702c3af7ae736e66c1532fbeb0c5e1e0226445
Marine Leaute <leautemarine@yahoo.fr>
2018-08-14 13:09:30 +0200

    seeds & pictures

--- app/models/rapper.rb
+++ app/models/rapper.rb
@@ -1,4 +1,6 @@
 class Rapper < ApplicationRecord
   belongs_to :agent, class_name: "User"
   has_many :bookings
+  mount_uploader :picture, PhotoUploader
+  mount_uploader :background_picture, PhotoUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,23 @@ puts "Creating users..."
 lisiane = User.create!(
   name: "lisiane",
   email: "lisiane@gmail.com",
-  password: "azerty"
+  password: "azerty",
+  picture: File.open(Rails.root.join("db/fixtures/users/lisiane.jpg"))
 )
 
 marine = User.create!(
   name: "Marine",
   email: "marine@gmail.com",
-  password: "azerty"
+  password: "azerty",
+  picture: File.open(Rails.root.join("db/fixtures/users/marine.jpg"))
 )
 
 enzo = User.create!(
-  name: "Enzo"
-  email: "enzo@gmail.com"
-  password: "azerty"
+  name: "Enzo",
+  email: "enzo@gmail.com",
+  password: "azerty",
+  picture: File.open(Rails.root.join("db/fixtures/users/enzo.jpg"))
+
   )
 
 puts "Creating rappers..."
@@ -35,10 +39,25 @@ booba = Rapper.create!({
   blaz: "Booba",
   description: "Grand black",
   punchline: "Que des n°10 dans ma team",
-  style: "french",
+  style: "hardcore",
   origin: "93",
   address: "25 boulevard de Stalingrad, 44000 Nantes, France",
-  price_per_day: 1000,
+  price_per_day: 5000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/booba.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/booba.jpg"))
+})
+
+vybz = Rapper.create!({
+  agent: marine,
+  blaz: "Vybz Kartell",
+  description: "Dance hall hardcore",
+  punchline: "Up & down like a yoyo",
+  style: "Dance hall",
+  origin: "Jamaican",
+  address: "52 rue Gambetta 44000, France",
+  price_per_day: 3000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/vybz.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/vybz.jpg"))
 })
 
 drake = Rapper.create!({
@@ -49,10 +68,12 @@ drake = Rapper.create!({
   style: "canadien",
   origin: "Vancouver",
   address: "2 Rue meuris, 44100 Nantes, France",
-  price_per_day: 4000,
+  price_per_day: 10000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/drake.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/drake.jpg"))
 })
 
-2pac = Rapper.create!({
+tupac = Rapper.create!({
   agent: lisiane,
   blaz: "2pac",
   description: "mort",
@@ -60,7 +81,9 @@ drake = Rapper.create!({
   style: "Hip-hop",
   origin: "Las Vegas",
   address: "8, allée de la maison rouge 44000 Nantes, France",
-  price_per_day: 700,
+  price_per_day: 20000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/2pac.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/2pac.jpg"))
 })
 
 teki = Rapper.create!({
@@ -71,9 +94,10 @@ teki = Rapper.create!({
   style: "electro",
   origin: "parisien",
   address: "24 rue kervegan 44000 Nantes, France",
-  price_per_day: 500,
+  price_per_day: 1000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/teki.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/teki.jpg"))
 })
-
 
 mc_circulaire = Rapper.create!({
   agent: lisiane,
@@ -84,6 +108,8 @@ mc_circulaire = Rapper.create!({
   origin: "Vendéen",
   address: "5 boulevard de Vendée 44200 Nantes, France",
   price_per_day: 100,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/mc-circulaire.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/mc-circulaire.jpg"))
 })
 
 jul = Rapper.create!({
@@ -95,20 +121,9 @@ jul = Rapper.create!({
   origin: "Marseille",
   address: "12 avenue des cigales, 44200 Nantes, France",
   price_per_day: 10,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/jul.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/jul.jpg"))
 })
-
-
-kanye = Rapper.create!({
-  agent: enzo,
-  blaz: "Kanye West",
-  description: "...",
-  punchline: "...",
-  style: "...",
-  origin: "...",
-  address: "5 rue de l'Ouest 44100 Nantes, France",
-  price_per_day: 666,
-})
-
 
 hamza = Rapper.create!({
   agent: marine,
@@ -119,19 +134,22 @@ hamza = Rapper.create!({
   origin: "Belge",
   address: "94, rue des hauts pavés 44000 Nantes, France",
   price_per_day: 1500,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/hamza.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/hamza.jpg"))
 })
 
 pnl = Rapper.create!({
   agent: enzo,
   blaz: "PNL",
-  description: "...",
+  description: "nouvelle vague",
   punchline: "Non mais allo",
   style: "Rap cheulou",
   origin: "Essone",
   address: "56 rue de Strasbourg 44000 Nantes, France",
   price_per_day: 10000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/pnl.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/pnl.jpg"))
 })
-
 
 sch = Rapper.create!({
   agent: marine,
@@ -142,6 +160,8 @@ sch = Rapper.create!({
   origin: "Marseille",
   address: "30 boulevard de la prairie au Duc 44200 Nantes, France",
   price_per_day: 4000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/sch.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/sch.jpg"))
 })
 
 ichon = Rapper.create!({
@@ -153,19 +173,95 @@ ichon = Rapper.create!({
   origin: "Paris",
   address: "3 rue des carmes 44000 Nantes, France",
   price_per_day: 1000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/ichon.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/ichon.jpg"))
 })
 
+liza = Rapper.create!({
+  agent: enzo,
+  blaz: "Liza Monet",
+  description: "rappeuse",
+  punchline: "Yaourt au fruit",
+  style: "hardcore",
+  origin: "panam",
+  address: "2 boulevard Jean Monnet 44000 Nantes, France",
+  price_per_day: 900,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/liza-monnet.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/liza-monnet.jpg"))
+})
 
+amandine = Rapper.create!({
+  agent: enzo,
+  blaz: "Amandine du 38",
+  description: "swag",
+  punchline: "swag",
+  style: "du 38",
+  origin: "Isère",
+  address: "20, rue Pitre Chevalier 44000 Nantes, France",
+  price_per_day: 200,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/amandine.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/amandine.jpg"))
+})
+
+notorious = Rapper.create!({
+  agent: enzo,
+  blaz: "Notorious Big",
+  description: "swag",
+  punchline: "...",
+  style: "East Coast",
+  origin: "Brooklyn",
+  address: "8 allée d'Orléan 44000 Nantes, France",
+  price_per_day: 3000,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/notorious.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/notorious.jpg"))
+})
+
+craig  = Rapper.create!({
+  agent: marine,
+  blaz: "Craig David",
+  description: "Meilleur son 2000",
+  punchline: "7 days",
+  style: "UK garage",
+  origin: "Londres",
+  address: "40 rue des hauts pavés 44000 Nantes, France",
+  price_per_day: 800,
+  picture: File.open(Rails.root.join("db/fixtures/rappers/craig.jpg")),
+  background_picture: File.open(Rails.root.join("db/fixtures/rappers/craig.jpg"))
+})
 
 
 puts "Creating bookings..."
 Booking.create!({
-  starting_on: DateTime.new(2018, 8, 1),
-  ending_on: DateTime.new(2018, 8, 2),
+  starting_on: Date.new(2018, 8, 1),
+  ending_on: Date.new(2018, 8, 2),
   user: marine,
   rapper: booba,
   status: "accepted",
   total_price: 6000
 })
 
+Booking.create!({
+  starting_on: Date.new(2018, 9, 1),
+  ending_on: Date.new(2018, 9, 5),
+  user: lisiane,
+  rapper: teki,
+  status: "pending",
+  total_price: 6000
+})
+Booking.create!({
+  starting_on: Date.new(2018, 8, 20),
+  ending_on: Date.new(2018, 8, 24),
+  user: enzo,
+  rapper: hamza,
+  status: "pending",
+  total_price: 6000
+})
+Booking.create!({
+  starting_on: Date.new(2018, 8, 1),
+  ending_on: Date.new(2018, 8, 2),
+  user: lisiane,
+  rapper: craig,
+  status: "accepted",
+  total_price: 6000
+})
 puts "Finished!"
