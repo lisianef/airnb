c5ceb83b6f5ff1bd91a639dd6de8693d5466bc47
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-14 16:34:03 +0200

    index avancé

--- app/controllers/rappers_controller.rb
+++ app/controllers/rappers_controller.rb
@@ -2,8 +2,12 @@ class RappersController < ApplicationController
   def index
     @rappers = Rapper.all
     # @const_map = new GMaps({ el: '#map', lat: 48.8648482, lng: 2.3798534, zoom: 14 })
+    # if params[:query].present?
+    #   @rapper = Rapper.where("blaz ILIKE ?", "%#{params[:query]}%")
+    # else
+    #   @rappers = Rapper.all
+    # end
   end
-
 
   def show
   end
