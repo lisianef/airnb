5da57276db56ad87a0c3b23792cde96e0bf79091
Enzo Mathe <enzomathe@gmail.com>
2018-08-15 12:54:32 +0200

    link homepage to rapper

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Need Love ? ", "Candies ?", "Hugs ?","Teddy Bears ?", "This is the place to be !"],
+    strings: ["Need Love ? ", "Candies ?", "Hugs ?","Teddy Bears ?", "You are at the right place !"],
     typeSpeed: 50,
     loop: true
   });
