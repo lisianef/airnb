7e71150a48fec8e24dfeb2177618d8f1050c765c
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-15 11:49:56 +0200

    wip add map

--- app/controllers/rappers_controller.rb
+++ app/controllers/rappers_controller.rb
@@ -1,11 +1,19 @@
 class RappersController < ApplicationController
   def index
-    @rappers = Rapper.all
-    if params[:query].present?
-      @rapper = Rapper.where("blaz ILIKE ?", "%#{params[:query]}%")
-    else
-      @rappers = Rapper.all
+    @rappers = Rapper.where.not(latitude: nil, longitude: nil)
+    @markers = @rappers.map do |rapper|
+      {
+        lat: rapper.latitude,
+        lng: rapper.longitude,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
     end
+    # @rappers = Rapper.all
+    # if params[:query].present?
+    #   @rapper = Rapper.where("blaz ILIKE ?", "%#{params[:query]}%")
+    # else
+    #   @rappers = Rapper.all
+    # end
   end
 
   def show
