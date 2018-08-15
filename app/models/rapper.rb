4858384b832abf57691efbcb6f59f7ff955b5234
Enzo Mathe <enzomathe@gmail.com>
2018-08-15 14:50:27 +0200

    I don't why i'm doing this

--- app/models/rapper.rb
+++ app/models/rapper.rb
@@ -1,4 +1,7 @@
 class Rapper < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :agent, class_name: "User"
   has_many :bookings
   mount_uploader :picture, PhotoUploader
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_160105) do
+ActiveRecord::Schema.define(version: 2018_08_15_093002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2018_08_14_160105) do
     t.integer "agent_id"
     t.string "picture"
     t.string "background_picture"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["agent_id"], name: "index_rappers_on_agent_id"
   end
 
