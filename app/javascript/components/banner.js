e8e298f2aad7febc7a473a312590dbc2e0a7eb0c
Enzo Mathe <enzomathe@gmail.com>
2018-08-16 11:23:58 +0200

    Finish style application

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,8 +3,8 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   if (document.getElementById("banner-typed-text")) {
     new Typed('#banner-typed-text', {
-      strings: ["Need Love", "Hugs", "You are at the right place"],
-      typeSpeed: 50,
+      strings: ["Need Love ?", "Hugs ?", "Teddy Bears ?", "You are at the right place", "Check out what we can offer you !"],
+      typeSpeed: 60,
       loop: true
     });
   }
