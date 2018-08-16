e7ab5177a10d2f98de2b9d59bba134f5ea85f255
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-16 16:39:23 +0200

    ajouter un rapper

--- app/controllers/agent/rappers_controller.rb
+++ app/controllers/agent/rappers_controller.rb
@@ -1,5 +1,25 @@
 class Agent::RappersController < ApplicationController
- def index
-   @rappers = current_user.rappers
- end
+  def index
+    @rappers = current_user.rappers
+  end
+
+  def new
+    @rapper = Rapper.new
+  end
+
+  def create
+    @rapper = Rapper.new(rapper_params)
+    @rapper.agent = User.find(current_user.id)
+    if @rapper.save
+      redirect_to agent_rappers_path(@rapper)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rapper_params
+    params.require(:rapper).permit(:blaz, :description, :punchline, :style, :origin, :price_per_day, :picture)
+  end
 end
