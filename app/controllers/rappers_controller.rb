13b48a5db1203b9b886860b924f0df926f0d6767
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-16 11:11:56 +0200

    Ajout de la search mais sans css pour le moment

--- app/controllers/rappers_controller.rb
+++ app/controllers/rappers_controller.rb
@@ -1,23 +1,36 @@
 class RappersController < ApplicationController
   def index
-    @rappers = Rapper.where.not(latitude: nil, longitude: nil)
-    @markers = @rappers.map do |rapper|
-      {
-        lat: rapper.latitude,
-        lng: rapper.longitude,
-        infoWindow: { content: render_to_string(partial: "/rappers/map_box", locals: { rapper: rapper }) }
-      }
-    end
-    # @rappers = Rapper.all
-    # if params[:query].present?
-    #   @rapper = Rapper.where("blaz ILIKE ?", "%#{params[:query]}%")
-    # else
-    #   @rappers = Rapper.all
+    # @rappers = Rapper.where.not(latitude: nil, longitude: nil)
+    # @markers = @rappers.map do |rapper|
+    #   {
+    #     lat: rapper.latitude,
+    #     lng: rapper.longitude,
+    #     infoWindow: { content: render_to_string(partial: "/rappers/map_box", locals: { rapper: rapper }) }
+    #   }
     # end
+    if params[:query].present?
+      @rappers = Rapper.where("blaz ILIKE ?", "%#{params[:query]}%").where.not(latitude: nil, longitude: nil)
+      @markers = marquer(@rappers)
+    else
+      @rappers = Rapper.all.where.not(latitude: nil, longitude: nil)
+      @markers = marquer(@rappers)
+    end
   end
 
   def show
     @rapper = Rapper.find(params[:id])
   end
 
+  private
+
+  def marquer(rappers)
+    rappers.map do |rapper|
+      {
+        lat: rapper.latitude,
+        lng: rapper.longitude,
+        infoWindow: { content: render_to_string(partial: "/rappers/map_box", locals: { rapper: rapper }) }
+      }
+    end
+    # @rappers = Rapper.where.not(latitude: nil, longitude: nil)
+  end
 end
