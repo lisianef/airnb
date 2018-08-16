7a53504991d7c2d8989509ca623a0938e96abb56
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-16 17:45:01 +0200

    add destroy

--- app/controllers/agent/rappers_controller.rb
+++ app/controllers/agent/rappers_controller.rb
@@ -1,5 +1,11 @@
 class Agent::RappersController < ApplicationController
- def index
-   @rappers = current_user.rappers
- end
+  def index
+    @rappers = current_user.rappers
+  end
+
+  def destroy
+    @rapper = Rapper.find(params[:id])
+    @rapper.destroy
+    redirect_to agent_rappers_path
+  end
 end
