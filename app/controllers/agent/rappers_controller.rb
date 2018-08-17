1b765e9ff275b5efd092a54c4cb9e3b564dd88ef
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-17 14:29:06 +0200

    edit

--- app/controllers/agent/rappers_controller.rb
+++ app/controllers/agent/rappers_controller.rb
@@ -1,8 +1,10 @@
 class Agent::RappersController < ApplicationController
+  before_action :set_rapper, only: [:edit, :update, :destroy]
+
   def index
     @rappers = current_user.rappers
   end
-  
+
   def new
     @rapper = Rapper.new
   end
@@ -17,13 +19,28 @@ class Agent::RappersController < ApplicationController
     end
   end
 
+  def edit
+    # @rapper = Rapper.find(params[:id])
+  end
+
+  def update
+    # @rapper = Rapper.find(params[:id])
+    @agent = User.find(current_user.id)
+    @rapper.update(rapper_params)
+    redirect_to agent_rappers_path(@rapper), :notice => "Your changes have been saved"
+  end
+
   def destroy
-    @rapper = Rapper.find(params[:id])
+    # @rapper = Rapper.find(params[:id])
     @rapper.destroy
     redirect_to agent_rappers_path, :notice => "Your rapper has been deleted"
   end
-  
+
   private
+
+  def set_rapper
+    @rapper = Rapper.find(params[:id])
+  end
 
   def rapper_params
     params.require(:rapper).permit(:blaz, :description, :punchline, :style, :origin, :price_per_day, :picture)
